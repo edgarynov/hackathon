f2ef52bc2c0a0855da5f7689d0b4f2d8a74f7b00
Christianngono <christianngonoabanda384@gmail.com>
2024-02-08 13:10:27 +0100

    To update sorter.go

--- hackathon/sorter.go
+++ hackathon/sorter.go
@@ -6,31 +6,40 @@ import (
 	"path/filepath"
 )
 
-func sortFiles(rootDir string) {
+func sortFiles(rootDir string) error {
 	// Implement file sorting logic, you can use filepath.Walk to navigate directory and sort files
 	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
 		// Ignore hidden folders and files
-		if info.IsDir() && info.Name()[0] == '.' {
-			return filepath.SkipDir
-		} else if info.Name()[0] == '.' {
+		if filepath.Base(path)[0] == '.' {
 			return nil
 		}
-		// move the file to a folder based on its extension
-		extension := filepath.Ext(info.Name())
-		// create a folder with the extension name in root dir if it doesn't exist already
-		extensionDir := filepath.Join(rootDir, extension[1:])
-		if _, err := os.Stat(extensionDir); os.IsNotExist(err) {
-			os.Mkdir(extensionDir, os.ModePerm)
+		if info.IsDir() {
+			// Ignore directories
+			return nil
 		}
-		// move the file to the folder
-		err = os.Rename(path, filepath.Join(extensionDir, info.Name()))
+		// Get the file extension
+		ext := filepath.Ext(path)
+		// Create a folder with the file extension
+		dir := filepath.Join(rootDir, ext[1:])
+		// Create the folder if it doesn't exist already
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			os.Mkdir(dir, 0755)
+		} else if err != nil {
+			return err
+		}
+		// Move the file to the folder
+		newPath := filepath.Join(dir, filepath.Base(path))
+		err = os.Rename(path, newPath)
 		if err != nil {
-			fmt.Println(err)
+			return err
 		}
+		fmt.Printf("Moved %s to %s\n", path, newPath)
 		return nil
-
 	})
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
+
+	fmt.Println("Files sorted successfully")
+	return nil
 }
